0c21116771d9c15a503760a8e030a9004e794d89
ppfuark <phfuark2007@gmail.com>
2026-04-06 16:06:32 -0300

    [feat]: Reading and streaming registro

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:vita_log/controller/hive_registro_controller.dart';
+import 'package:vita_log/models/registro.dart';
 import 'package:vita_log/pages/widgets/new_registro.dart';
 
 class Home extends StatefulWidget {
@@ -16,6 +20,10 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    final HiveRegistroController hiveRegistroController =
+        HiveRegistroController(context: context);
+    final List<Registro> registros = hiveRegistroController.fetch();
+
     final w = MediaQuery.of(context).size.width;
     final h = MediaQuery.of(context).size.height;
 
@@ -30,9 +38,43 @@ class _HomeState extends State<Home> {
         width: w,
         height: h,
         decoration: BoxDecoration(),
-        child: Column(children: [
-        
-        ],),
+        child: Column(
+          children: [
+            SizedBox(
+              height: h * .8,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final e = registros[index];
+                  return ListTile(
+                    leading: e.imagePath != null
+                        ? Image.file(File(e.imagePath!))
+                        : null,
+                    title: Text(e.type),
+                    subtitle: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(e.humorLevel.toString()),
+                        e.humorLevel == 0
+                            ? Text('😭')
+                            : e.humorLevel == 1
+                            ? Text('😞')
+                            : e.humorLevel == 2
+                            ? Text('😥')
+                            : e.humorLevel == 3
+                            ? Text('😐')
+                            : e.humorLevel == 4
+                            ? Text('😊')
+                            : Text('😄'),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: registros.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
